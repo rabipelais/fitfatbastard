3b21817623c4a5e29a3eb5d2a1436949ee16fddd
Sebastian Mendez <sebas.chinoir@gmail.com>
2015-12-01 21:13:24 +0100

    Copy over stuff from `event` tutorial project.
    Text fields for adding event name and location, date input, and button
    to add to listings.

--- src/clojure/my/stuff/fitlogger/main.clj
+++ src/clojure/my/stuff/fitlogger/main.clj
@@ -28,23 +28,84 @@
              (res/get-string R$string/your_input_fmt input))
            :long)))
 
+
+(defn get-elmt [activity elmt]
+  (str (.getText ^TextView (find-view activity elmt))))
+
+(defn set-elmt [activity elmt s]
+  (on-ui (config (find-view activity elmt) :text s)))
+
+(defn format-events [events]
+  (->>
+   (map (fn [[location event]]
+            (format "%s - %s\n" location event))
+          events)
+   (join "                        ")))
+
+(defn format-listing [lst]
+  (->> (map (fn [[date events]]
+            (format "%s - %s" date (format-events events)))
+          lst)
+     join))
+
+(def listing (atom (sorted-map)))
+
+(defn update-ui [activity]
+  (set-elmt activity ::listing (format-listing @listing))
+  (set-elmt activity ::location "")
+  (set-elmt activity ::name ""))
+
+(defn add-event [activity]
+  (let [date-key (try
+                   (read-string (get-elmt activity ::date))
+                   (catch RuntimeException e "Date string is empty!"))]
+    (when (number? date-key)
+      (swap! listing update-in [date-key] (fnil conj [])
+             [(get-elmt activity ::location) (get-elmt activity ::name)])
+      (update-ui activity))))
+
+(defn date-picker [activity]
+  (proxy [DialogFragment DatePickerDialog$OnDateSetListener] []
+    (onCreateDialog [savedInstanceState]
+      (let [c (Calendar/getInstance)
+            year (.get c Calendar/YEAR)
+            month (.get c Calendar/MONTH)
+            day (.get c Calendar/DAY_OF_MONTH)]
+        (DatePickerDialog. activity this year month day)))
+    (onDateSet [view year month day]
+      (set-elmt activity ::date
+                (format "%d%02d%02d" year (inc month) day)))))
+
+(defn show-picker [^Activity activity dp]
+  (. dp show (. activity getFragmentManager) "datePicker"))
+
+(defn main-layout [activity]
+  [:linear-layout {:orientation :vertical}
+   [:edit-text {:hint "Event name",
+                :id ::name}]
+   [:edit-text {:hint "Event location",
+                :id ::location}]
+   [:linear-layout {:orientation :horizontal}
+    [:text-view {:hint "Event date",
+                 :id ::date}]
+    [:button {:text "...",
+              :on-click (fn [_] (show-picker activity
+                                            (date-picker activity)))}]]
+   [:button {:text "+ Event",
+             :on-click (fn [_]
+                         (add-event activity))}]
+   [:text-view {:text (format-listing @listing),
+                :id ::listing}]])
+
 ;; This is how an Activity is defined. We create one and specify its onCreate
 ;; method. Inside we create a user interface that consists of an edit and a
 ;; button. We also give set callback to the button.
-(defactivity my.stuff.fitlogger.MainActivity
+(defactivity my.stuff.events.MainActivity
   :key :main
 
   (onCreate [this bundle]
     (.superOnCreate this bundle)
     (neko.debug/keep-screen-on this)
     (on-ui
-      (set-content-view! (*a)
-        [:linear-layout {:orientation :vertical
-                         :layout-width :fill
-                         :layout-height :wrap}
-         [:edit-text {:id ::user-input
-                      :hint "Type text here"
-                      :layout-width :fill}]
-         [:button {:text R$string/touch_me ;; We use resource here, but could
-                                           ;; have used a plain string too.
-                   :on-click (fn [_] (notify-from-edit (*a)))}]]))))
+     (set-content-view! (*a) (main-layout (*a)))
+     (set-elmt (*a) ::listing (format-listing @listing)))))
